a9064c1028ffbd6f259c427313711bbd9bdadca2
tmwnd <timwende040501@outlook.de>
2022-01-08 13:59:39 +0100

    db: utf8mb4

--- db/sql/clear.sql
+++ db/sql/clear.sql
@@ -1,13 +1,5 @@
+drop database if exists pokemon;
+create database pokemon;
 use pokemon
-
-drop table if exists attacke_tm;
-drop table if exists spiel_generation;
-drop table if exists lernt;
-drop table if exists attacke;
-drop table if exists entwicklung;
-drop table if exists item;
-drop table if exists pokemon;
-drop table if exists effektivitaet;
-drop table if exists typ;
 
 source .\pokemon.sql
